4d343f8287a1b8d253814f9e0ad3707231dcc7de
Prem Santosh Udaya Shankar <premsantosh.udayashankar@cerner.com>
2013-12-18 15:59:19 -0600

    Initial commit, added few functions

--- src/chiffonier/core.clj
+++ src/chiffonier/core.clj
@@ -1,6 +1,39 @@
-(ns chiffonier.core)
+(ns chiffonier.core
+  (:use [clojure.java.shell :only [sh]])
+  (:use [clojure.string :only [split]])
+  (:import [java.io File FileNotFoundException])
+  (:require [clojure.java.io :as cio]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn pwd []
+  "Returns the currect working directory"
+  (->
+    (sh "pwd")
+    (second)
+    (second)
+    (split #"\n")
+    (first)))
+
+(defn ls [path]
+  "Returns a list of stirngs of files and
+    directories in the provided path"
+  (.list (File. path)))
+
+(defn mkdir [dir_name]
+  "Creates the directory and returns true
+    if created successfully"
+  (.mkdir (File. dir_name)))
+
+(defn mkpath [path]
+  "Created the path recursively and returns true
+    if successful else returns the error occurred"
+  (let [ret (sh "mkdir" "-p" path)]
+    (if (= (:exit ret) 0) (Boolean. true) (:err ret))))
+
+
+
+
+
+
+
+
