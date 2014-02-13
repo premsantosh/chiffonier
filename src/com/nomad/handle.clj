127c405e26591cbfc0848c5ae6e1a7d92f77dde1
Prem Santosh Udaya Shankar <premsantosh.udayashankar@cerner.com>
2014-02-13 09:53:53 -0600

    Added functionallity to force delete files/dir and to return basename

--- src/com/nomad/handle.clj
+++ src/com/nomad/handle.clj
@@ -5,9 +5,9 @@
            [java.lang System])
   (:require [clojure.java.io :as cio]))
 
-
-(defn pwd []
+(defn pwd
   "Returns the current working directory"
+  []
   (->
     (sh "pwd")
     (second)
@@ -15,56 +15,79 @@
     (split #"\n")
     (first)))
 
-(defn ls [path]
+(defn ls
   "Returns a list of strings of files and
     directories in the provided path"
+  [path]
   (.list (File. path)))
 
-(defn mkdir [dir_name]
+(defn mkdir
   "Creates the directory and returns true
     if created successfully"
-  (.mkdir (File. dir_name)))
+  [dir-name]
+  (.mkdir (File. dir-name)))
 
-(defn mkpath [path]
+(defn mkpath
   "Created the path recursively and returns true
     if successful else returns the error occurred"
+  [path]
   (let [ret (sh "mkdir" "-p" path)]
     (if (= (:exit ret) 0) (Boolean. true) (:err ret))))
 
-(defn rm [name]
+(defn rm
   "Deletes the file or directory (only if not empty)
     and returns true if successful"
+  [name]
   (.delete (File. name)))
 
-(defn exist? [name]
+(defn rm-force
+  "Force deletes file or directory and returns
+  the return code (0 if true)"
+  [name]
+  (->
+   (sh "rm" "-rf" name)
+   (first)))
+
+(defn exist?
   "Returns true if the file or directory exists
     in the given path else returns false"
+  [name]
   (.exists (File. name)))
 
-(defn cmod [user group other path_to_file]
+(defn cmod
   "Modifies the file permissions for the specified file
     with the given user group and other privilidges. The
     user group and other are decimal values"
+  [user group other path-to-file]
   (let [privs (str user group other)
-        ret (sh "chmod" privs path_to_file)]
+        ret (sh "chmod" privs path-to-file)]
     (if (= (:exit ret) 0) (Boolean. true) (:err ret))))
 
-(defn last-modified [file]
+(defn last-modified
   "Returns the last modified time (in milli seconds) of the file"
+  [file]
   (.lastModified (File. file)))
 
-(defn size [file]
+(defn size
   "Returns the size of the file in bytes"
+  [file]
   (if (exist? file)
       (.length (File. file))
       (throw (FileNotFoundException. (str "Couldn't locate the file" file)))))
 
-(defn file-join [file_one file_two]
-  "Joins two paths to create a true unified path"
-  (let [one (clojure.string/replace file_one #"/+$" "")
-        two (clojure.string/replace file_two #"^/+" "")]
-    (str one "/" two)))
+(defn file-join
+  "Joins multiple paths to create a true unified path"
+  [& files]
+  (let [new-files (map (fn [file] (clojure.string/replace file #"/" "")) files)]
+    (reduce (fn [a b] (str a "/" b)) new-files)))
 
-(defn home []
+(defn home
   "Returns the home for the current user"
+  []
   (System/getProperty "user.home"))
+
+(defn basename
+  "Returns the base file name given a path
+  to a file"
+  [path]
+  (last (clojure.string/split path #"/")))
--- test/com/nomad/t_core.clj
+++ test/com/nomad/t_core.clj
@@ -4,10 +4,13 @@
             [com.nomad.handle :refer :all]))
 
 (fact "File join will occur with proper forward slashes"
-      (let [result (file-join "foo///" "//bar")]
-        result => "foo/bar"))
+      (let [result (file-join "foo///" "//bar" "/test/")]
+        result => "foo/bar/test"))
 
 (fact "Testing the size of license file as it will not change"
       (size (str (pwd) "/" "LICENSE")) => 11220)
+
+(fact "Testing the basename function"
+      (basename "/home/code/foo/bar.clj") => "bar.clj")
 
 
